976295c7e2891c1865a5faa29e6c4cbbc94aabd6
jelatta <jlatta6@gmail.com>
2023-02-21 19:34:59 -0500

    added a namespace

---
+++ pysnipeit/build/lib/pysnipeit/assets.py
@@ -0,0 +1,20 @@
+
+
+class Assets(object):
+    def __init__(self, apibase):
+        self.apibase = apibase
+        self.base_uri = '/api/v1/hardware'
+    
+    def list(self, **kwargs):
+        uri = self.base_uri        
+        return self.apibase._list(uri, **kwargs)
+    
+    def details(self, id ):
+        uri = f'{self.base_uri}/{id}'
+        return self.apibase.invoke_api(uri)
+    
+    def update(self, data):
+        uri = self.base_uri
+        self.apibase.invoke_api(uri, method='post', data=data)
+
+
--- pysnipeit/setup.py
+++ pysnipeit/setup.py
@@ -6,12 +6,15 @@ with open('README.md') as fh:
     
 setup(
     name='pysnipeit',
-    version='0.0.1',
+    version='0.0.2',
     description='Python library to interface with SNIPE_IT\'s API',
     author='Jeff Latta',
+    long_description=LONG_DESCRIPTION,
     url='',
     classifiers=[],
     packages=find_packages(),
     install_requires=['requests'],
+    extras_require={}
+
 
 )
