68b309ebfc4d848f7d7cc877c97329a58abf89d1
jelatta <jlatta6@gmail.com>
2023-03-07 18:20:57 -0500

    more custom tags

--- bootstrap_snipeit/bootstrap_infra.py
+++ bootstrap_snipeit/bootstrap_infra.py
@@ -22,19 +22,59 @@ infra = {
                 'name':'Computers'
             },
         'fields':
-            {
+            [{
                 'name':'OS',
                 'element': 'text',
                 'format' : 'ANY'
-            }
+            },
+            {
+                'name':'ip',
+                'element': 'text',
+                'format' : 'IP'
+            },
+            {
+                'name':'software',
+                'element': 'text',
+                'format' : 'ANY'
+            },
+            {
+                'name':'aws_ami_id',
+                'element': 'text',
+                'format' : 'ANY'
+            },
+             {
+                'name':'aws_vpc_id',
+                'element': 'text',
+                'format' : 'ANY'
+            },
+            {
+                'name':'mac_address',
+                'element': 'text',
+                'format' : 'ANY'
+            },
+            {
+                'name':'sources',
+                'element': 'text',
+                'format' : 'ANY'
+            },
+            ]
         }
+
+
+def add_field(data):
+    fields = [snipe.fields.update(field) for field in data]
+    fieldsets = next(i for i in snipe.fieldsets.list() if i['name'] == infra['fieldsets']['name'])
+    for field in fields:
+        snipe.fields.associate(field_id=field['id'], fieldset_id=fieldsets['id'])
+    
 
 def main():
     manufacturers = snipe.manufacturers.update(infra.get('manufacturers'))    
     categories = snipe.categories.update(infra.get('categories'))  
-    fields = snipe.fields.update(infra.get('fields'))
+    fields = [snipe.fields.update(field) for field in infra.get('fields')]
     fieldsets = snipe.fieldsets.update(infra.get('fieldsets'))
-    snipe.fields.associate(field_id=fields['id'], fieldset_id=fieldsets['id'])
+    for field in fields:
+        snipe.fields.associate(field_id=field['id'], fieldset_id=fieldsets['id'])
     infra['models']['category_id'] = categories['id']
     infra['models']['manufacturer_id'] =manufacturers['id']
     infra['models']['fieldset_id'] = fieldsets['id']
